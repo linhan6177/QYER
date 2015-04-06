8e7a2838fe366702b3880e5b998f5dfc2d98ed8f
linhan <lwc6177@qq.com>
2015-04-06 12:55:15 +0800

    Initial Commit

---
+++ QYer/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  QYer
+//
+//  Created by linhan on 15-4-6.
+//  Copyright (c) 2015年 linhan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
